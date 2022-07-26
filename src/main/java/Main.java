eb4ea86f0adec664f244638e01dcefa6b58f6a83
jeremyzaire <jz.jeremyzaire@gmail.com>
2022-07-27 00:53:50 +0200

    Solution of the challenge

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Main {
+import fr.jeremyzaire.challenge.TypeWriter;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TypeWriter typeWriter = new TypeWriter();
+        typeWriter.draw("2* 15sp 1x 4sQ nl");
+        typeWriter.draw("1sp 1/ 1bS 1_ 1/ 1bS nl 1( 1sp 1o 1. 1o 1sp 1) nl 1sp 1> 1sp 1^ 1sp 1< nl 2sp 3|");
+
+        // EOF
+        System.out.println();
+    }
 }
--- src/main/java/fr/jeremyzaire/challenge/TypeWriter.java
+++ src/main/java/fr/jeremyzaire/challenge/TypeWriter.java
@@ -1,6 +1,69 @@
 package fr.jeremyzaire.challenge;
 
-public class Draw {
+import java.util.Arrays;
+import java.util.List;
 
+/**
+ * Class used to print ascii art.
+ */
+public class TypeWriter {
 
+    public void draw(String recipes) {
+        for (String code: splitRecipes(recipes.strip())) {
+            print(code);
+        }
+    }
+
+    /**
+     * Get abbreviations from ascii code.
+     * @param code The ascii code.
+     * @return The expected abbreviaton.
+     */
+    public String getAbbreviation(String code) {
+        if (code != null) {
+            return switch(code) {
+                case "sp" -> code.replace("sp", " ");
+                case "bS" -> code.replace("bS", "\\");
+                case "sQ" -> code.replace("sQ", "'");
+                case "nl" -> code.replace("nl", "\n");
+                default -> "";
+            };
+        }
+
+        return "";
+    }
+
+    /**
+     * Used to split recipes in little code ascii.
+     * @param recipes The original recipes.
+     * @return A list of code ascii.
+     */
+    public List<String> splitRecipes(String recipes) {
+        return List.of(recipes.split("\\s"));
+    }
+
+    /**
+     * Used to print code ascii with occurence.
+     * For exemple : 123 means 333333333333
+     * @param code The ascii code.
+     */
+    public void print(String code) {
+        int factor = 1;
+        String str;
+
+        List<String> abbreviations = Arrays.asList("sp", "bS", "sQ", "nl");
+        if(abbreviations.stream().anyMatch(code::contains)) {
+            str = getAbbreviation(code.substring(code.length()-2));
+            if(code.length() > 2) {
+                factor = Integer.parseInt(code.substring(0, code.length()-2));
+            }
+        } else {
+            str = String.valueOf(code.charAt(code.length()-1));
+            factor = Integer.parseInt(code.substring(0, code.length()-1));
+        }
+
+        for(int i=0; i<factor; i++) {
+            System.out.print(str);
+        }
+    }
 }
